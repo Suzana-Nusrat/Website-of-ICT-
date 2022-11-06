d27886d9f997cc4f38c5b4b24ef67ceb844e69f9
Anika Raisa <76096958+anikaraisa11@users.noreply.github.com>
2022-11-06 11:42:16 +0600

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,66 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CustomAuthController;
-use Illuminate\Support\Facades\Route\View;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ContactUsFormController;
-use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
 */
 
-Route::get('dashboard', [CustomAuthController::class, 'dashboard']);
-Route::get('login', [CustomAuthController::class, 'index'])->name('login');
-Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
-Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
-Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
-Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
-
-
-Route::get("/",[HomeController::class,"index"]);
-
-Route::get("/redirects",[HomeController::class,"redirects"]);
-
-Route::get('/contact', [ContactUsFormController::class, 'createForm']);
-Route::post('/contact', [ContactUsFormController::class, 'ContactUsForm'])->name('contact.store');
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route::post('/', StudentController::class);
+// Route::controller(StudentController::class)->group(function () {
+//     Route::get('/student', 'show');
+//     Route::post('/student', 'store');
+// });
 
 Route::resource("/student", StudentController::class);
-
-Route::get('/index', function () {
-    return view('index');
-});
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/people', function () {
-    return view('people');
-});
-Route::get('/contact', function () {
-    return view('contact');
-});
-Route::get('/research', function () {
-    return view('research');
-});
-Route::get('/fundedProject', function () {
-    return view('fundedProject');
-});
-Route::get('/publication', function () {
-    return view('publication');
-});
-Route::get('/newseve', function () {
-    return view('newseve');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
